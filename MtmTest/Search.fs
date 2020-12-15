2e30155950541b6b61c0c6352b219466b456adef
Aron Adler <aron@adler.dev>
2020-12-15 23:13:38 +0000

    Try working through logic but get stuck & comment

--- MtmTest/Search.fs
+++ MtmTest/Search.fs
@@ -3,13 +3,11 @@
 open HierarchyTree
 
 
-type SearchResultString =
-    | Unmarked of string
-    | Marked of string
+type SearchResultChar = Unmarked of char | Marked of char
 
 /// String consisting of some marked and some unmarked parts,
 /// the marked parts correlating to the query string
-type MarkedString = SearchResultString list
+type MarkedString = SearchResultChar list
 
 
 let printMarkedString (markedString : MarkedString) =
@@ -17,11 +15,43 @@ let printMarkedString (markedString : MarkedString) =
     |> List.fold
         (fun strSoFar thisChunk ->
             match thisChunk with
-            | Unmarked str -> strSoFar + str
-            | Marked str -> strSoFar + (sprintf "<mark>%s</mark>" str))
+            | Unmarked str -> strSoFar + string str
+            | Marked str -> strSoFar + (sprintf "<mark>%c</mark>" str)) // this marks every char, even adjacent ones, but not a big deal; can optimise later
         ""
 
 
 type SearchResultTree =
-    | Branch of MarkedString * SearchResultTree list
-    | Leaf of MarkedString
+    | ResultBranch of MarkedString * SearchResultTree list
+    | ResultLeaf of MarkedString
+
+
+
+
+(*
+We need to keep track of:
+ a. how much of query string we've been able to match
+ b. marked target string so far
+ c. the target string left
+*)
+
+
+/// Very much in progress
+let findInStr queryStr targetStr =
+    targetStr
+    |> List.fold
+        (fun (coveredQueryStr,yetToDoQueryStr,markedString) targetStrChar ->
+            match yetToDoQueryStr with
+            | [] -> ()
+            | list -> ()
+
+        )
+        ((List.empty,queryStr),List.empty)
+
+
+let findInHierarchyTree queryStr (tree : HierarchyTree<string>) =
+    let rec traverser remainingQueryStr remainingTree =
+        match remainingTree with
+        | Leaf str -> ()
+
+
+
