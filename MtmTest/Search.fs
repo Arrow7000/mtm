fcaa31c7ab2f208dd78cf42134c428f602048457
Aron Adler <aron@adler.dev>
2020-12-15 23:51:41 +0000

    Complete func for traversing hierarchy tree
    and returning a search result tree

--- MtmTest/Search.fs
+++ MtmTest/Search.fs
@@ -22,7 +22,7 @@ let printMarkedString (markedString : MarkedString) =
 
 type SearchResultTree =
     | ResultBranch of MarkedString * SearchResultTree list
-    | ResultLeaf of MarkedString
+    | ResultLeaf
 
 
 
@@ -51,15 +51,42 @@ let findInCharList queryStr targetStr =
 
 
 
-/// WIP
+/// Straight conversion of a hierarchy tree to a search result tree, for when
+/// all descendants of a tree need to be converted, e.g. when the whole rest
+/// of the tree is to be included in the search results.
+let rec convertHierarchyToSearchResult (tree : HierarchyTree<string>) =
+    match tree with
+    | Leaf str ->
+        let markedStr = Seq.toList str |> List.map Unmarked
+        ResultBranch (markedStr, List.empty)
+    | Branch (str, children) ->
+        let markedStr = Seq.toList str |> List.map Unmarked
+        ResultBranch (markedStr, List.map convertHierarchyToSearchResult children )
+
+
+
 let findInHierarchyTree queryStr (tree : HierarchyTree<string>) =
     let rec traverser remainingQueryCharList remainingTree =
         match remainingQueryCharList with
-        | [] -> remainingTree // 
+        | [] -> convertHierarchyToSearchResult remainingTree // If everything in query string has been matched then return all descendants
         | remainingQuery ->
             match remainingTree with
             | Leaf str ->
-                findInCharList remainingQueryCharList
+                let targetCharList = Seq.toList str
+                let (queryCharsLeft,markedString) = findInCharList remainingQuery targetCharList
 
+                match queryCharsLeft with
+                | [] -> // this branch matches!
+                    ResultBranch (markedString, List.empty)
+                | _ -> ResultLeaf // this branch doesn't match!
+            | Branch (str,children) ->
+                let targetCharList = Seq.toList str
+                let (queryCharsLeft,markedString) = findInCharList remainingQuery targetCharList
+
+                match queryCharsLeft with
+                | [] ->
+                    ResultBranch (markedString, List.map convertHierarchyToSearchResult children)
+                | someLeft ->
+                    ResultBranch (markedString, List.map (traverser someLeft) children)
 
     traverser (Seq.toList queryStr) tree
