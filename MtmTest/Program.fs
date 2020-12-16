823b4d5c5ec507f85a676ab89dd5988ae2011a90
Aron Adler <aron@adler.dev>
2020-12-16 20:57:53 +0000

    Add web server and serialisation logic

--- MtmTest/Program.fs
+++ MtmTest/Program.fs
@@ -3,14 +3,41 @@
 open System.IO
 open HierarchyTree
 open Search
+open Serialisation
+open Suave
+open Suave.Operators
+open Suave.Filters
+
+
+let handler hierarchyTree (req : HttpRequest) =
+    match req.queryParam "q" with
+    | Choice1Of2 queryStr ->
+        findInHierarchyTrees queryStr hierarchyTree
+        |> serialiseQueryResponse queryStr
+        |> jsonToStr
+        |> Successful.OK
+    | Choice2Of2 _ ->
+        RequestErrors.BAD_REQUEST """A query parameter of "q" is required """
+
+
 
 [<EntryPoint>]
-let main argv =
-   
-    File.ReadAllText "pairs.csv"
-    |> parseCsv
-    |> makeTree
-    |> findInHierarchyTrees "medical shit"
-    |> printfn "%A"
+let main _ =
+    let hierarchyTree =
+        File.ReadAllText "pairs.csv"
+        |> parseCsv
+        |> makeTree
+
+    let handler' = handler hierarchyTree
+    let endpoint : WebPart =
+        path "/search/data-categories" >=> GET
+        >=> Writers.setMimeType "application/json; charset=utf-8"
+        >=> request handler'
+
+    let config =
+        { defaultConfig with
+            bindings = [ HttpBinding.create HTTP Net.IPAddress.Any (uint16 4000) ] }
+
+    startWebServer config endpoint
 
     0 // return an integer exit code
---
+++ MtmTest/Serialisation.fs
@@ -0,0 +1,25 @@
+﻿module Serialisation
+
+open FSharp.Data
+open Search
+
+let private serialiseList = List.toArray >> JsonValue.Array
+
+
+let rec serialiseResponseTreeToJson (responseTree : ResponseTree) =
+    match responseTree with
+    | ChildResult str -> JsonValue.String str
+    | ParentResult (str, children) ->
+        [| "name", JsonValue.String str
+           "children", List.map serialiseResponseTreeToJson children |> serialiseList |]
+        |> JsonValue.Record
+
+
+
+let serialiseQueryResponse queryStr responseTrees =
+    [| "searchTerm", JsonValue.String queryStr 
+       "results", responseTrees |> List.map serialiseResponseTreeToJson |> serialiseList |]
+    |> JsonValue.Record
+
+
+let jsonToStr (jsonValue : JsonValue) = jsonValue.ToString()
